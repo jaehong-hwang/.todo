c94274f9ff8b9f7664c82c660457a3b688206fb4
Jaehong Hwang <dev.jaehong@gmail.com>
2022-02-02 02:00:17 +0900

    response값 없으면 추가되도록 수정

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"os"
 
 	"github.com/jaehong-hwang/todo/cli"
+	r "github.com/jaehong-hwang/todo/response"
 )
 
 func main() {
-	if response, isJson := cli.Run(os.Args); response != nil {
+	if response, isJson := cli.Run(os.Args); response != nil || isJson {
+		if isJson && response == nil {
+			response = &r.MessageResponse{Message: "empty response"}
+		}
 		response.Print(isJson)
 	}
 }
